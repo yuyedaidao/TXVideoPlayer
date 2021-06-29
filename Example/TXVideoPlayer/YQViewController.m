8268882308bc8c4c9d05f126bfaa44d396ae8b9b
WYQ <wyqpadding@gmail.com>
2021-06-29 11:56:13 +0800

    update

--- Example/TXVideoPlayer/YQViewController.m
+++ Example/TXVideoPlayer/YQViewController.m
@@ -12,7 +12,7 @@
 @import Masonry;
 
 @interface YQViewController ()
-
+@property (strong, nonatomic) SuperPlayerView* player;
 @end
 
 @implementation YQViewController
@@ -21,6 +21,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = UIColor.whiteColor;
     SuperPlayerView *view = [[SuperPlayerView alloc] init];
     [self.view addSubview:view];
     [view mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -29,13 +30,24 @@
     }];
     view.backgroundColor = UIColor.blackColor;
     SuperPlayerView *player = view;
-    player.fatherView = self.view;
-    player.autoPlay = YES;
-    SuperPlayerModel *model = [[SuperPlayerModel alloc] init];
-    model.videoURL = @"http://200024424.vod.myqcloud.com/200024424_709ae516bdf811e6ad39991f76a4df69.f20.mp4";
-    [player playWithModel:model];
+//    player.fatherView = self.view;
+//    player.autoPlay = YES;
+    
+    self.player = player;
     
 }
+
+- (IBAction)prepareAction:(id)sender {
+    self.player.autoPlay = false;
+    SuperPlayerModel *model = [[SuperPlayerModel alloc] init];
+    model.videoURL = @"http://200024424.vod.myqcloud.com/200024424_709ae516bdf811e6ad39991f76a4df69.f20.mp4";
+    [self.player playWithModel:model];
+}
+- (IBAction)playAction:(id)sender {
+    [self.player resume];
+}
+
+
 
 - (void)didReceiveMemoryWarning
 {
